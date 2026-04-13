111b9d94d28507482e81edf207002636ba7207af
Hamka-akbari03 <hamkaakbari41@gmail.com>
2026-04-13 21:46:00 +0800

    Add files via upload

---
+++ Modul1/PRAK101.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 1 - Soal 1</title>
+</head>
+<body>
+
+    <?php
+    echo "Hello World in PHP<br>";
+    echo "Nama Praktikan: Muhammad Hamka Akbari<br>";
+    echo "NIM: 2410817110007<br>";
+    ?>
+</body>
+</html>
